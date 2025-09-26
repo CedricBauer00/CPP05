07fda59b4799fd4eae5068286d3a11c075e52503
Cedric Bauer <cbauer@3-C-1.42heilbronn.de>
2025-09-26 16:00:52 +0200

    Working on the ex02 since this morning, forgot to commit throughout; need to still implement the other subcalsses; adding tests to the main

---
+++ ex02/inc/AForm.hpp
@@ -0,0 +1,46 @@
+# pragma once
+
+#include <string>
+#include <iostream>
+
+class Bureaucrat;
+
+class AForm
+{
+    private:
+        const std::string   _name;
+        bool                _isSigned;
+        const int           _gradeToSign;
+        const int           _gradeToExecute;
+    public:
+        AForm();
+        AForm( const std::string name, int gradeToSign, int gradeToExecute );
+        AForm( const AForm& copy );
+        AForm& operator=( const AForm& copy );
+        ~AForm();
+    
+        std::string     getAFormName() const;
+        bool            getIsSigned() const;
+        int             getGradeToSign() const;
+        int             getGradeToExecute() const;
+        void            beSigned( const Bureaucrat& bureaucrat ); // Form wird signiert wenn Grade hoch genug
+        virtual void    execute( Bureaucrat const & executor ) const = 0; // <- pure virtual
+        
+        class FormNotSignedException : public std::exception
+        {
+            public:
+                const char* what() const throw();
+        };
+        class GradeTooHighException : public std::exception
+        {
+            public:
+                const char* what() const throw();
+        };
+        class GradeTooLowException : public std::exception
+        {
+            public:
+                const char* what() const throw();
+        };
+};
+
+std::ostream& operator<<( std::ostream& os, const AForm& form );
---
+++ ex02/src/main.cpp
@@ -0,0 +1,87 @@
+#include "../inc/Bureaucrat.hpp"
+#include "../inc/Form.hpp"
+
+int main()
+{
+    try
+    {
+        Bureaucrat  alfred("Alfred", 100);
+        Form        contract( "Contract", 100, 50);
+        std::cout   << "Before signing: "
+                    << contract 
+                    << std::endl;
+        alfred.signForm( contract );
+        std::cout   << "After signing: "
+                    << contract 
+                    << std::endl;
+    }
+    catch ( Bureaucrat::GradeTooHighException &e ) //catch( std::exception &e )???
+    {
+        std::cout << e.what() << std::endl;
+    }
+    catch ( Bureaucrat::GradeTooLowException &e )
+    {
+        std::cout << e.what() << std::endl;
+    }
+    try
+    {
+        Bureaucrat  intern( "Intern", 100);
+        Form        confidentialinfo( "confidentialinfo", 5, 1 );
+        std::cout   << "Before signing: "
+                    << confidentialinfo
+                    << std::endl;
+        intern.signForm( confidentialinfo );
+        std::cout   << "After signing: "
+                    << confidentialinfo
+                    << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cout << e.what() << '\n';
+    }
+    try
+    {
+        std::cout << "\n--- Testing copy and assignment operator ---\n" << std::endl;
+        Bureaucrat doc("Doc", 30);
+        Bureaucrat copyDoc(doc);
+        Bureaucrat assignemnt("AssignDoc", 150);
+        assignemnt = doc;
+
+        std::cout << "Original doc: " << doc << std::endl;
+        std::cout << "Copy of doc: " << copyDoc << std::endl;
+        std::cout << "Assingment: " << assignemnt << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+    try
+    {
+        std::cout << "\n--- Testing Form copy and assignment operator ---\n" << std::endl;
+        Bureaucrat  doc("Doc", 20);
+        Form Ticket("Ticket", 50, 25);
+        Form copyTicket(Ticket);
+        Form assignmentForm("Temp", 150, 150);
+        assignmentForm = Ticket;
+
+        std::cout << "-------Before signing--------" << std::endl;
+        std::cout << "Original Ticket: " << Ticket << std::endl;
+        std::cout << "CopyTicket: " << copyTicket << std::endl;
+        std::cout << "AssingmentTicket: " << assignmentForm << std::endl;
+        
+        doc.signForm(Ticket);
+
+        std::cout << "-------After signing--------" << std::endl;
+        std::cout << "Original Ticket: " << Ticket << std::endl;
+        assignmentForm = Ticket;
+        std::cout << "AssignmentTicket: " << assignmentForm << std::endl;
+        Form secondCopy(Ticket);
+        std::cout << "SecondCopy:" << secondCopy << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cout << e.what() << '\n';
+    }
+    
+    return (0);
+}
